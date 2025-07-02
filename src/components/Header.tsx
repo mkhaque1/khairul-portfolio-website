081497158881d91f187780915aee74ef2f26a097
mkhaque1 <mkhaque29@gmail.com>
2025-07-02 18:55:46 +0200

    update cv path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
 
   const handleDownloadCV = () => {
     const link = document.createElement('a');
-    link.href = 'src/public/M M Khairul Haque_CV_2025.pdf';
+    link.href = '/M M Khairul Haque_CV_2025.pdf';
     link.download = 'M_M_Khairul_Haque_CV.pdf';
     link.click();
   };
